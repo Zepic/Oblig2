4ddfcbad27273e14fba852567d58d04c7251a578
Zepic <horto@hotmail.no>
2020-04-27 15:30:40 +0200

    a

---
+++ controller.js
@@ -0,0 +1,24 @@
+//let clicked = 0;
+
+/*function selectedPole(pole){
+    if (currentPole == pole) return currentPole = null
+    else isSelected(pole)
+
+    
+}*/
+function isSelected(pole){
+    if (currentPole != pole) currentPole = pole
+    else currentPole = null
+    show()
+    console.log(currentPole)
+}
+function addPole(){
+
+}
+function changeValuePole(){
+
+}
+
+function removePole(){
+
+}
