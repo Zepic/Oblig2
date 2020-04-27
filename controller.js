4e2e6760ee88d6c4121222bd93f3b4f486c96677
Zepic <horto@hotmail.no>
2020-04-27 15:31:05 +0200

    Update controller.js
    removes useless code

--- controller.js
+++ controller.js
@@ -1,11 +1,4 @@
-//let clicked = 0;
 
-/*function selectedPole(pole){
-    if (currentPole == pole) return currentPole = null
-    else isSelected(pole)
-
-    
-}*/
 function isSelected(pole){
     if (currentPole != pole) currentPole = pole
     else currentPole = null
