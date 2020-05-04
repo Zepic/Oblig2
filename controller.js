fdf4ead2d234be72a0445d56567daf4156e39c96
Zepic <horto@hotmail.no>
2020-05-04 12:10:33 +0200

    Finished Oblig 2

--- controller.js
+++ controller.js
@@ -1,17 +1,33 @@
-
+let userValue;
 function isSelected(pole){
-    if (currentPole != pole) currentPole = pole
-    else currentPole = null
-    show()
-    console.log(currentPole)
+    if (currentPole != pole) currentPole = pole;
+    else currentPole = null;
+    show();
 }
+
+function poleValue(userInput){
+    userValue = userInput;
+}
+
 function addPole(){
-
+    if (userValue == null) return alert("There is no number");
+    else if (userValue > 10) return alert("The number is too high");
+    numbers.push(userValue);
+    if (currentPole != "ingen") currentPole = currentPole + 1;
+    userValue = null;
+    show();
 }
-function changeValuePole(){
 
+function changeValueOfPole(){
+    if (userValue == null) return alert("There is no number");
+    else if (userValue > 10) return alert("The number is too high");
+    numbers[currentPole - 1] = userValue;
+    userValue = null;
+    show();
 }
 
-function removePole(){
-
+function removePole(index){
+numbers.splice(index -1 , 1);
+currentPole = null;
+show();
 }
